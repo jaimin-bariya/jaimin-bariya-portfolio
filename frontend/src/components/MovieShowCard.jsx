a492fbd2a8e77abf17c9100ea9951d90e3ba5bd9
JaiminBariya <jaiminbariyastudent@gmail.com>
2025-01-09 15:49:30 +0530

    Added movies to adbout page

--- frontend/src/components/MovieShowCard.jsx
+++ frontend/src/components/MovieShowCard.jsx
@@ -8,7 +8,7 @@ const MovieShowCard = ({movie}) => {
       <div>
         
             <div className="border dark:border-white border-black rounded-t-xl w-60 h-auto overflow-hidden ">
-            {/* <img className="w-full h-full" src={movie['poster']} alt="" /> */}
+            <img className="w-full h-full" src={movie['poster']} alt="" />
             <p className=" p-4 text-center">{movie['name']}</p>
             
             </div>
