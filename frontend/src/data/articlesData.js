1c05aa0518a85a47e3afbc1615d6760e84782fec
Jaimin Bariya <117170778+jaimin-bariya@users.noreply.github.com>
2025-01-25 13:54:49 +0530

    Update data source of articlesData

--- frontend/src/data/articlesData.js
+++ frontend/src/data/articlesData.js
@@ -1,29 +1,64 @@
 /** @format */
 
 const Toparticles = [
-  {
-    title: "SQL Basic Question for interview",
-    date: "November 2024 ",
-    href: "https://dev.to/jaiminbariya/sql-basic-question-for-interview-51j5",
-  },
-  {
-    title: "SQL Basic and Intermediate Questions for Interview",
-    date: "December 2024",
-    href: "https://dev.to/jaiminbariya/sql-basic-and-intermediate-questions-for-interview-3jgo",
-  },
-  {
-    title: "Implementation of ACID transaction in Database",
-    date: "October 2024",
-    href: "https://dev.to/jaiminbariya/implementation-of-acid-transaction-in-database-44nc",
-  },
-  {
-    title: "Isolation in ACID Transaction",
-    date: "October 2024",
-    href: "https://dev.to/jaiminbariya/isolation-in-acid-transaction-3055",
-  },
+    {
+        title: "SQL Basic Question for interview",
+        date: "November 2024 ",
+        href: "https://dev.to/jaiminbariya/sql-basic-question-for-interview-51j5",
+    },
+    {
+        title: "SQL Basic and Intermediate Questions for Interview",
+        date: "December 2024",
+        href: "https://dev.to/jaiminbariya/sql-basic-and-intermediate-questions-for-interview-3jgo",
+    },
+        {
+        title: "Python Complexity Cheat Sheet",
+        date: "January 2025",
+        href: "https://dev.to/jaiminbariya/python-complexity-cheat-sheet-3plf",
+        },
+    {
+        title: "Why Quick Sort is Faster Than Heap Sort",
+        date: "January 2025",
+        href: "https://dev.to/jaiminbariya/why-quick-sort-is-faster-than-heap-sort-4aca",
+    },
+    {
+        title: "Implementation of ACID transaction in Database",
+        date: "October 2024",
+        href: "https://dev.to/jaiminbariya/implementation-of-acid-transaction-in-database-44nc",
+    },
+    {
+        title: "Isolation in ACID Transaction",
+        date: "October 2024",
+        href: "https://dev.to/jaiminbariya/isolation-in-acid-transaction-3055",
+    },
 ];
 
 const AllArticles = {
+  
+  2025: [
+    {
+    title: "Why Quick Sort is Faster Than Heap Sort",
+    date: "January 2025",
+    href: "https://dev.to/jaiminbariya/why-quick-sort-is-faster-than-heap-sort-4aca",
+    },
+    {
+    title: "371 SUM OF TWO INTEGERS - leetcode - Python",
+    date: "January 2025",
+    href: "https://dev.to/jaiminbariya/371-sum-of-two-integers-leetcode-python-11ff",
+    },
+    {
+    title: "Python Complexity Cheat Sheet",
+    date: "January 2025",
+    href: "https://dev.to/jaiminbariya/python-complexity-cheat-sheet-3plf",
+    },
+    {
+    title: "Leetcode Blind 75",
+    date: "January 2025",
+    href: "https://dev.to/jaiminbariya/leetcode-blind-75-49mp",
+    },
+  ],
+
+
   2024: [
     {
       title: "SQL Basic Question for interview",
