8ec74191dfec48f8f5bcc0f5d872a9c5378414b6
Jaimin Bariya <117170778+jaimin-bariya@users.noreply.github.com>
2025-03-19 23:00:42 +0530

    Update data source of miscellaneousData

--- frontend/src/data/miscellaneousData.js
+++ frontend/src/data/miscellaneousData.js
@@ -11,6 +11,11 @@ const YTChannels = [
     link: "https://www.youtube.com/@takeUforward",
     for: "DSA",
   },
+    {
+    channelName: "CampusMonk",
+    link: "https://www.youtube.com/@campusmonk",
+    for: "Quantitative Aptitude, Logical Reasoning, Sankalp Batch", 
+  },
 ];
 
 export { YTChannels };
