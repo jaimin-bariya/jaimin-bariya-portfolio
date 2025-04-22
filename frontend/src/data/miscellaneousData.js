03e120be2db3a32621878e4dceb6a37a29aa9a20
Jaimin Bariya <117170778+jaimin-bariya@users.noreply.github.com>
2025-04-22 15:43:50 +0530

    Update data source of miscellaneousData

--- frontend/src/data/miscellaneousData.js
+++ frontend/src/data/miscellaneousData.js
@@ -16,6 +16,17 @@ const YTChannels = [
     link: "https://www.youtube.com/@campusmonk",
     for: "Quantitative Aptitude, Logical Reasoning, Sankalp Batch", 
   },
+    {
+    channelName: "LangChain Academy",
+    link: "https://academy.langchain.com/courses/intro-to-langgraph",
+    for: "Introduction to LangGraph", 
+  },
+
+    {
+    channelName: "Dr. Angela Yu - Udemy",
+    link: "https://www.udemy.com/course/100-days-of-code/",
+    for: "100 Days of Code: The Complete Python Pro Bootcamp", 
+  },
 ];
 
 export { YTChannels };
